f296f6a69eac5b1673336172cce143c87e3471af
Talden Key <tayho3893@gmail.com>
2020-08-31 19:40:01 -0700

    forgot to save

---
+++ CarpetCalculator.java
@@ -0,0 +1,13 @@
+public class CarpetCalculator{
+
+    public int RoomDimension {
+        int legend= 9;
+        Area = length * width
+        return int;
+    }
+
+    public int RoomCarpet (RoomDimension){
+        RoomDimension*CarpetCost
+        return TotalCost;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+/*
+Name: Tay Ho
+CptS 233: MicroAssignment #1
+Date: 08/31/20
+gitRepo url:https://github.com/Talden-Key/Micro-Assignment1.git
+*/
+
+public void main(String[] args) {
+    int ma = 0;
+}
