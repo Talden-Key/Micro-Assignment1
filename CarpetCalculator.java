85615de6abbe4c53d81a0c6a60165c918b035196
TaDelk <tayho3893@gmail.com>
2020-09-02 22:04:27 -0700

    Completed

--- CarpetCalculator.java
+++ CarpetCalculator.java
@@ -1,13 +1,16 @@
 public class CarpetCalculator{
+int Area;
+int TotalCost;
+int CarpetCost = 8;
 
-    public int RoomDimension {
-        int legend= 9;
-        Area = length * width
-        return int;
+    public int RoomDimension (int length,int width){
+        
+        return length * width;
     }
 
-    public int RoomCarpet (RoomDimension){
-        RoomDimension*CarpetCost
+    public int RoomCarpet (int length, int width){
+        Area = RoomDimension(length, width);
+        TotalCost = Area * CarpetCost;
         return TotalCost;
     }
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,31 @@
-/*
+import java.util.*;/*
 Name: Tay Ho
 CptS 233: MicroAssignment #1
 Date: 08/31/20
 gitRepo url:https://github.com/Talden-Key/Micro-Assignment1.git
 */
+public class Main{
+    
 
-public void main(String[] args) {
-    int ma = 0;
+    public static void main(String[] args) {
+        int lengthInput;
+        int widthInput;
+        int Cost;
+
+        // creat object type carpetcalculor 
+        CarpetCalculator carpetCalculator = new CarpetCalculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Bovine Carpet Corp");
+        System.out.println("We only sell one type one price carpet of 8$/sprtft.");
+        System.out.println("What is the length and width of the area you want to cover with carpet?");
+        System.out.println("Length (meter): ");
+        lengthInput = scanner.nextInt();
+        System.out.println("Width (meter): ");
+        widthInput = scanner.nextInt();
+        
+        // Calculate the cost
+        Cost = carpetCalculator.RoomCarpet(lengthInput, widthInput);
+        System.out.println("Here is your estimate cost: ");
+        System.out.println("$ " + Cost);
+    }
 }
